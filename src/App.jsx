bc540f8c0effc4c0abaa9e03f6db486937b97347
chandan <chandankumar6204995@gmail.com>
2025-10-04 14:49:41 +0530

    Setup protected routes with redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Login from './components/Login.jsx'
 import './App.css'
 import Signup from './components/Signup.jsx'
-import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      
-    </Routes>
     </>
   )
 }
